751eb07408b451e3dfb168a3fabdca59c9aa8b37
Sebastian Pandu W <89185866+babastian18@users.noreply.github.com>
2023-11-03 09:47:04 +0700

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Starting deployment process on the target VM..."
-# Add your deployment commands here
-docker-compose up -d
+#deployment commands
+# Pull the latest Docker image
+docker pull 21052002/solitaire-test:latest
+# Run the Docker container
+docker run -d -p 8000:5000 21052002/solitaire-test:latest
 
 echo "Deployment process completed."
     
