2e5abfbce00aefd7644462581659422db7bfd3f0
Sebastian Pandu W <89185866+babastian18@users.noreply.github.com>
2023-11-03 11:38:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/welcome/')
 @app.route('/welcome/<nama>')
-def welcome(nama):
-    if nama.isalpha():
+def welcome(nama=None):
+    if nama and nama.strip():
         return f"Selamat datang, {nama}!"
     else:
         return "Anonymous"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
